7ddd78cd9185d11c12c768f84d72b76feedd5ed8
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2023-04-23 11:22:59 -0300

    create home auth screen

---
+++ app/src/main/java/com/example/movieapp/presenter/auth/home/HomeAuthFragment.kt
@@ -0,0 +1,18 @@
+package com.example.movieapp.presenter.auth.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.movieapp.R
+
+class HomeAuthFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home_auth, container, false)
+    }
+}
