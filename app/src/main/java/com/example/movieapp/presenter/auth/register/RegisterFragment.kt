bcda3a45b00e993a4e56bb7392a76f97e993025b
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2023-04-23 12:33:48 -0300

    create register screen

---
+++ app/src/main/java/com/example/movieapp/presenter/auth/register/RegisterFragment.kt
@@ -0,0 +1,19 @@
+package com.example.movieapp.presenter.auth.register
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.movieapp.R
+
+class RegisterFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_register, container, false)
+    }
+}
