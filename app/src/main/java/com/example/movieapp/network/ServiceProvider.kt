a1302f4249d1967e82385b788fba293a81835f7f
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2023-04-25 14:58:35 -0300

    config retrofit http client

---
+++ app/src/main/java/com/example/movieapp/network/ServiceProvider.kt
@@ -0,0 +1,27 @@
+package com.example.movieapp.network
+
+import com.example.movieapp.BuildConfig
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+class ServiceProvider {
+
+    private val client = OkHttpClient.Builder()
+        .connectTimeout(30, TimeUnit.SECONDS)
+        .readTimeout(30, TimeUnit.SECONDS)
+        .addInterceptor(HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        })
+        .build()
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(BuildConfig.BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .client(client)
+        .build()
+
+    fun <T> createService(apiClass: Class<T>): T = retrofit.create(apiClass)
+}
