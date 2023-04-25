5073a7774e5b77c6ea435ecfc5fe81704e0b147d
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2023-04-25 17:34:53 -0300

    verify logged user

--- app/src/main/java/com/example/movieapp/presenter/auth/activity/AuthActivity.kt
+++ app/src/main/java/com/example/movieapp/presenter/auth/activity/AuthActivity.kt
@@ -1,5 +1,6 @@
 package com.example.movieapp.presenter.auth.activity
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
@@ -7,6 +8,8 @@ import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.example.movieapp.R
 import com.example.movieapp.databinding.ActivityAuthBinding
+import com.example.movieapp.presenter.main.activity.MainActivity
+import com.example.movieapp.util.FirebaseHelper
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -24,6 +27,7 @@ class AuthActivity : AppCompatActivity() {
 
         setStatusBarTranslucent()
         initNavigation()
+        isAuthenticated()
     }
 
     private fun initNavigation() {
@@ -43,5 +47,12 @@ class AuthActivity : AppCompatActivity() {
             View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
                     View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
                     View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+    }
+
+    private fun isAuthenticated() {
+        if (FirebaseHelper.isAuthenticated()) {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
     }
 }
