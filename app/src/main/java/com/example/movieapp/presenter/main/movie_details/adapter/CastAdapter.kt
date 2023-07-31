550808b35676b59f120bd43a41bd6f53a02d6671
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2023-07-31 09:17:18 -0300

    53 - adapter listagem de atores

---
+++ app/src/main/java/com/example/movieapp/presenter/main/movie_details/adapter/CastAdapter.kt
@@ -0,0 +1,47 @@
+package com.example.movieapp.presenter.main.movie_details.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.movieapp.databinding.CastItemBinding
+import com.example.movieapp.domain.model.Person
+
+class CastAdapter : ListAdapter<Person, CastAdapter.MyViewHolder>(DIFF_CALLBACK) {
+
+    companion object {
+        val DIFF_CALLBACK = object : DiffUtil.ItemCallback<Person>() {
+            override fun areItemsTheSame(oldItem: Person, newItem: Person): Boolean {
+                return oldItem.id == newItem.id
+            }
+
+            override fun areContentsTheSame(oldItem: Person, newItem: Person): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        return MyViewHolder(
+            CastItemBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val person = getItem(position)
+
+        Glide.with(holder.binding.root.context)
+            .load("https://image.tmdb.org/t/p/w500${person.profilePath}")
+            .into(holder.binding.imgCast)
+
+        holder.binding.tvPersonName.text = person.name
+    }
+
+    inner class MyViewHolder(val binding: CastItemBinding) : RecyclerView.ViewHolder(binding.root)
+}
