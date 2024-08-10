fdadc02bb7411394a6c094cac9bd7837530b033b
Carlos Henrique de S. Ferreira <carloshenrique4m@gmail.com>
2024-08-10 17:32:46 -0300

    328-criando caixa de pesquisa tela de busca

--- app/src/main/java/com/example/movieapp/presenter/main/bottombar/search/SearchFragment.kt
+++ app/src/main/java/com/example/movieapp/presenter/main/bottombar/search/SearchFragment.kt
@@ -5,14 +5,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.movieapp.R
+import com.example.movieapp.databinding.FragmentSearchBinding
 
 class SearchFragment : Fragment() {
+
+    private var _binding: FragmentSearchBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_search, container, false)
+    ): View {
+        _binding = FragmentSearchBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
