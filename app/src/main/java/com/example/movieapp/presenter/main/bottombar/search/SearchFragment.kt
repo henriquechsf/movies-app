1b4eab016404d0db8f4d1f5f25ec25f37d961147
Carlos Henrique de S. Ferreira <carloshenrique4m@gmail.com>
2024-08-10 23:31:54 -0300

    331-configurando listagem na tela de pesquisa

--- app/src/main/java/com/example/movieapp/presenter/main/bottombar/search/SearchFragment.kt
+++ app/src/main/java/com/example/movieapp/presenter/main/bottombar/search/SearchFragment.kt
@@ -1,14 +1,22 @@
 package com.example.movieapp.presenter.main.bottombar.search
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.SearchView
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.movieapp.MainGraphDirections
+import com.example.movieapp.R
 import com.example.movieapp.databinding.FragmentSearchBinding
-import com.example.movieapp.presenter.main.moviegenre.MovieGenreViewModel
+import com.example.movieapp.presenter.main.bottombar.home.adapter.MovieAdapter
 import com.example.movieapp.util.StateView
+import com.example.movieapp.util.hideKeyboard
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -18,6 +26,7 @@ class SearchFragment : Fragment() {
     private val binding get() = _binding!!
 
     private val viewModel: SearchViewModel by viewModels()
+    private lateinit var movieAdapter: MovieAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,22 +36,65 @@ class SearchFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initRecycler()
+        initSearchView()
+    }
+
+    private fun initRecycler() {
+        movieAdapter = MovieAdapter(
+            context = requireContext(),
+            layoutInflater = R.layout.movie_genre_item,
+            onMovieClickListener = { movieId ->
+                val action = MainGraphDirections.actionGlobalMovieDetailsFragment(movieId)
+                findNavController().navigate(action)
+            }
+        )
+
+        with(binding.rvMovies) {
+            layoutManager = GridLayoutManager(requireContext(), 2)
+            setHasFixedSize(true)
+            adapter = movieAdapter
+        }
+    }
+
+    private fun initSearchView() {
+        binding.searchView.setOnQueryTextListener(object :
+            SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                Log.d("SimpleSearchView", "Submit:$query")
+                hideKeyboard()
+                if (query.isNotEmpty()) {
+                    searchMovies(query)
+                }
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                Log.d("SimpleSearchView", "Text changed:$newText")
+                return false
+            }
+        })
+    }
+
     private fun searchMovies(query: String?) {
         viewModel.searchMovies(query).observe(viewLifecycleOwner) { stateView ->
             when (stateView) {
                 is StateView.Loading -> {
-                    //binding.rvMovies.isVisible = false
-                    //binding.progressBar.isVisible = true
+                    binding.rvMovies.isVisible = false
+                    binding.progressBar.isVisible = true
                 }
 
                 is StateView.Success -> {
-                    //binding.progressBar.isVisible = false
-                    //movieAdapter.submitList(stateView.data)
-                    //binding.rvMovies.isVisible = true
+                    binding.progressBar.isVisible = false
+                    movieAdapter.submitList(stateView.data)
+                    binding.rvMovies.isVisible = true
                 }
 
                 is StateView.Error -> {
-                    //binding.progressBar.isVisible = false
+                    binding.progressBar.isVisible = false
                 }
             }
         }
